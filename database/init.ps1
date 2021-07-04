28376b660740a50d0a42cb9157b7813180e7b6bc
Leonardi Fabianto <av.leonardif@gmail.com>
2021-07-04 22:37:00 +0700

    initialize repo

---
+++ database/init.ps1
@@ -0,0 +1,56 @@
+# DB Initialization Script
+# Description:
+#   Script to reconstruct Database, Schema and Admin for Inquisio. Configuration will be read from conf.txt.
+# Args:
+#   -
+# Prompt:
+#   - USER: str. Database superuser username
+#   - PASS: str. Database superuser password
+# Example:
+#   & .\init.ps1
+
+########## VARIABLE ##########
+Get-Content conf.txt | Foreach-Object{
+   $var = $_.Split('=')
+   New-Variable -Name $var[0] -Value $var[1]
+}
+$user = Read-Host "Superuser Username"
+$pass = Read-Host "Superuser Password" -AsSecureString
+$env:PGPASSWORD = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pass))
+$env:PGOPTIONS = "--client-min-messages=warning"
+
+########## MAIN ##########
+# Start execution logging
+$start_ut = $(Get-Date -UFormat %s)
+Write-Host "[$(Get-Date -UFormat '%Y-%m-%d %H:%M:%S')] Start $database database and admin initiation"
+
+# Delete existing database, schema and admin role for Inquisio
+psql -h $hostname -U $user -c "DROP DATABASE IF EXISTS $($database);" -q
+psql -h $hostname -U $user -c "DROP ROLE IF EXISTS $($admin_user);" -q
+
+# Create admin role
+psql -h $hostname -U $user -c "CREATE ROLE $($admin_user) WITH CREATEROLE LOGIN PASSWORD '$($admin_pass)';" -q
+Write-Host "[$(Get-Date -UFormat '%Y-%m-%d %H:%M:%S')] Succesfully create '$admin_user' as Inquisio admin"
+
+# Create database
+psql -h $hostname -U $user -c "CREATE DATABASE $($database);" -q
+psql -h $hostname -U $user -c "GRANT ALL PRIVILEGES ON DATABASE $($database) TO $($admin_user);" -q
+Write-Host "[$(Get-Date -UFormat '%Y-%m-%d %H:%M:%S')] Succesfully create '$database' database"
+
+# Create CDC schema
+$env:PGPASSWORD = $admin_pass
+psql -h $hostname -d $database -U $admin_user -c "CREATE SCHEMA $($schema_cdc);" -q
+Write-Host "[$(Get-Date -UFormat '%Y-%m-%d %H:%M:%S')] Succesfully create '$schema_cdc' schema as CDC schema"
+
+# Create Archive schema
+psql -h $hostname -d $database -U $admin_user -c "CREATE SCHEMA $($schema_arc);" -q
+Write-Host "[$(Get-Date -UFormat '%Y-%m-%d %H:%M:%S')] Succesfully create '$schema_arc' schema as Archive schema"
+
+# Clear environment variable
+Remove-Item Env:PGPASSWORD
+Remove-Item Env:PGOPTIONS
+
+# End execution logging
+$end_ut = $(Get-Date -UFormat %s)
+$duration = $($end_ut - $start_ut)
+Write-Host "[$(Get-Date -UFormat '%Y-%m-%d %H:%M:%S')] Succesfully reconstruct $database database, schema and admin role in $([int]$duration) second(s)."
