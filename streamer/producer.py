2093bfa814ee76d5fba56f2352bd1ab2409b16ba
Leonardi Fabianto <av.leonardif@gmail.com>
2021-07-15 07:37:42 +0700

    implement producer publish data

--- streamer/producer.py
+++ streamer/producer.py
@@ -1,26 +1,31 @@
 from kafka import KafkaProducer
+from kafka.errors import NoBrokersAvailable
+import time
 
 def get_producer(params):
-
+    """Get producer instance"""
     topic = params.get("TOPIC")
     bootstrap_server = params.get("BOOTSTRAP_SERVER")
-    _producer = KafkaProducer(bootstrap_server=bootstrap_server)
+    _producer = KafkaProducer(bootstrap_servers=[bootstrap_server])
 
     return Producer(_producer, topic)
 
 class Producer:
+    """KafkaProducer object wrapper for scraper result"""
     def __init__(self, producer, topic):
         self.producer = producer
         self.topic = topic
 
     def publish_data(self, data):
-        tries = 0
-        while(tries < 5):
+        tries = 1
+        data_byte = bytes(json.dumps(data), encoding="utf-8")
+        while(tries <= 5):
             try:
-                data_byte = bytes(str(data), encoding="utf-8")
-                self.producer.send(data_byte)
+                self.producer.send(self.topic, value=data_byte)
                 self.producer.flush()
-
                 break
-            except:
+
+            except NoBrokersAvailable as e:
+                print(f"Error while publishing data to {self.topic} topic. Retry in 5 seconds. Attempt : {tries}")
+                time.sleep(5)
                 tries += 1
